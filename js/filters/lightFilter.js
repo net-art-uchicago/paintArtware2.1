ae3dff9eca22c19a0b15c9b1d1648db87e4daaeb
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-10-11 15:49:36 -0500

    Light filter

--- js/filters/lightFilter.js
+++ js/filters/lightFilter.js
@@ -1,23 +1,23 @@
 window.filters.lightFilter = {
-    name: 'Light Filter',
-    menu: 'Edit',
-    run: function () {
-        const imageData = app.ctx.getImageData(0, 0, app.canvas.width, app.canvas.height)
-        const data = imageData.data
-        var brightness = 1.4
-        for (let i = 0; i < data.length; i += 4) {
-            var r = data[i]
-            var g = data[i + 1]
-            var b = data[i + 2]
+  name: 'Light Filter',
+  menu: 'Edit',
+  run: function () {
+    const imageData = app.ctx.getImageData(0, 0, app.canvas.width, app.canvas.height)
+    const data = imageData.data
+    const brightness = 1.4
+    for (let i = 0; i < data.length; i += 4) {
+      const r = data[i]
+      const g = data[i + 1]
+      const b = data[i + 2]
 
-            brightR = brightness * r
-            brightG = brightness * g
-            brightB = brightness * b
+      let brightR = brightness * r
+      let brightG = brightness * g
+      let brightB = brightness * b
 
-            data[i] = brightR
-            data[i + 1] = brightG
-            data[i + 2] = brightB
-          }
-        app.ctx.putImageData(imageData, 0, 0)
+      data[i] = brightR
+      data[i + 1] = brightG
+      data[i + 2] = brightB
     }
+    app.ctx.putImageData(imageData, 0, 0)
   }
+}
