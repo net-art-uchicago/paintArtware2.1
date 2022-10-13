4d9c6f4c8be63b43547f0e6c8f023349b354105d
Isaac Berman <berman.isaac01@gmail.com>
2022-10-12 23:59:33 -0500

    platforms ig. needs better collisions

--- js/tools/platformer.js
+++ js/tools/platformer.js
@@ -9,18 +9,39 @@ window.tools.platformer = {
     xPos: 0,
     yPos: 0,
     oldX: 0,
-    oldY: 0
+    oldY: 0,
+    gravity: 1 // note that this value is the downward force
   },
   events: {
     mousedown: function (e, self) {
       const mouse = app.eventToMouse(e)
       if (self.state.selected) {
-        console.log(mouse.x, mouse.y)
+        self.state.xPos = mouse.x
+        self.state.yPos = mouse.y
       }
     },
     mousemove: function (e, self) {
       function platformerLoop (self) {
-        console.log('plat loop', self.state.xPos++)
+        console.log('plat loop', self.state.xPos, self.state.yPos, self.state.selected)
+        if (self.state.selected) {
+          app.ctx.rect(self.state.xPos - 2, self.state.yPos - 2, 3, 3)
+          app.ctx.fill()
+          // check if touching a black pixel directly below
+          var grounded = false
+          const floorPixel = app.ctx.getImageData(self.state.xPos, self.state.yPos + 2, 1, 1).data
+          if (
+            floorPixel[0] < 120 &&
+            floorPixel[1] < 120 &&
+            floorPixel[2] < 120 &&
+            floorPixel[3] > 200) {
+            grounded = true
+          }
+          if (!grounded) {
+            self.state.yPos += self.state.gravity
+          } else {
+            console.log('grounded')
+          }
+        }
       }
       // using this as a dummy piece of code to run setup early on because
       // I don't have any better choices right now
@@ -28,7 +49,16 @@ window.tools.platformer = {
       if (self.state.setup === false) {
         self.state.setup = true
         console.log('hllow')
-        setInterval(platformerLoop, 20, self)
+        setInterval(platformerLoop, 2, self)
+      }
+    },
+    keydown: function (e, self) {
+      console.log('user pressed key', e.code)
+      if (e.code === 'ArrowLeft') {
+        self.state.xPos -= 1
+      }
+      if (e.code === 'ArrowRight') {
+        self.state.xPos += 1
       }
     }
   }
