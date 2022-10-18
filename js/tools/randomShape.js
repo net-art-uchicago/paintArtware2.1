097ddf3ad394046294121b362cf2c89309626f3e
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-10-18 15:30:13 -0500

    Finished randomShape for now...

--- js/tools/randomShape.js
+++ js/tools/randomShape.js
@@ -18,30 +18,26 @@ window.tools.randomShape = {
         const randomNumber = Math.floor(Math.random() * 3)
         const mouse = app.eventToMouse(e)
         const size = Math.abs(Math.sin(Date.now() / 500) * 50)
-        console.log(size)
         app.ctx.beginPath()
         if (randomNumber === 0) {
           app.ctx.arc(mouse.x, mouse.y, size, 0, 2 * Math.PI, false)
           app.ctx.fill()
           app.ctx.fillStyle = 'blue'
           app.ctx.stroke()
-          console.log('circle')
         }
         else if (randomNumber === 1) {
-          app.ctx.rect(mouse.x, mouse.y, mouse.x + size, mouse.y + size)
+          app.ctx.rect(mouse.x, mouse.y, (size * 2), (size * 2))
           app.ctx.fill()
           app.ctx.fillStyle = 'red'
           app.ctx.stroke()
-          console.log('rectangle')
         }
         else {
-          app.ctx.moveTo((size / 2) + 50, size / 2)
-          app.ctx.lineTo((size / 2), (size / 2) - 50)
-          app.ctx.lineTo((size / 2) - 50, size / 2)
+          app.ctx.moveTo(mouse.x + (size / 10) + 50, mouse.y + size / 10)
+          app.ctx.lineTo(mouse.x + (size / 8), mouse.y + (size / 8) - 50)
+          app.ctx.lineTo(mouse.x + (size / 8) - 50, mouse.y + (size / 8))
           app.ctx.fill()
           app.ctx.fillStyle = 'green'
           app.ctx.stroke()
-          console.log('triangle')
         }
       }
     }
